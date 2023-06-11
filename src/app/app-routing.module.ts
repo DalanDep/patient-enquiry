6331fe19c7b95ad80afc9109ee8a117551918628
DalanDep <danielramirezsanchez93@outlook.es>
2023-06-12 01:55:15 +0200

    Pages and routes are created
    All the pages that should be necessary for the application to work have been created.
    Routes have been created, adding all the patient's routes as children of layaut which works as the main one.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'patient',
+        loadChildren: () => import('./patient/patient.module').then(m => m.PatientModule)
+    },
+    {
+        path: 'error404',
+        component: Error404PageComponent
+    },
+    {
+        path: '',
+        redirectTo: 'patient',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'error404',
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/patient/patient-routing.module.ts
+++ src/app/patient/patient-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { DetailPageComponent } from './pages/detail-page/detail-page.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: LayoutPageComponent,
+        children: [
+            {
+                path: 'list',
+                component: ListPageComponent
+            },
+            {
+                path: ':id',
+                component: DetailPageComponent
+            },
+            {
+                path: '**',
+                redirectTo: 'list'
+            },
+        ]
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
 export class PatientRoutingModule { }
